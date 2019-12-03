a4f8114d7ef9ad080d6a31bb159b8a5e07639b23
Carlos Gomes Martinho <kmartinho8@gmail.com>
2019-12-03 08:13:34 +0100

    chore: remove cmake find package after installation

--- recipes/rocksdb/all/conanfile.py
+++ recipes/rocksdb/all/conanfile.py
@@ -78,6 +78,7 @@ class RocksDB(ConanFile):
                   src=self._source_subfolder)
         cmake = self._configure_cmake()
         cmake.install()
+        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
         self.cpp_info.name = "RocksDB"
