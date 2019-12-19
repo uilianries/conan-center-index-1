b9c9e5d0f006b24bdaa4aa1ae40e5282fa1676bc
Uilian Ries <uilianries@gmail.com>
2019-12-19 10:15:19 -0300

    Keep cppzmq simple
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/cppzmq/all/conanfile.py
+++ recipes/cppzmq/all/conanfile.py
@@ -1,5 +1,5 @@
 import os
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, tools
 
 
 class CppZmqConan(ConanFile):
@@ -9,8 +9,7 @@ class CppZmqConan(ConanFile):
     license = "MIT"
     topics = ("conan", "cppzmq", "zmq-cpp", "zmq", "cpp-bind")
     url = "https://github.com/conan-io/conan-center-index"
-    exports_sources = ["CMakeLists.txt", "cppzmq_extra.cmake"]
-    generators = "cmake", "cmake_find_package"
+    exports_sources = ["cppzmq_extra.cmake"]
     requires = "zeromq/4.3.2"
 
     @property
@@ -21,32 +20,14 @@ class CppZmqConan(ConanFile):
         tools.get(**self.conan_data["sources"][self.version])
         os.rename("cppzmq-{}".format(self.version), self._source_subfolder)
 
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["CPPZMQ_BUILD_TESTS"] = False
-        cmake.configure()
-        return cmake
-
-    def _patch_sources(self):
-        for patch in self.conan_data["patches"][self.version]:
-            tools.patch(**patch)
-
-    def build(self):
-        self._patch_sources()
-        cmake = self._configure_cmake()
-        cmake.build()
-
     def package(self):
         self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
-        cmake.install()
-
-        tools.rmdir(os.path.join(self.package_folder, "share"))
-        self.copy("cppzmq_extra.cmake", dst=os.path.join(self.package_folder, "lib", "cmake", "cppzmq"))
+        self.copy(pattern="*.hpp", dst="include", src=self._source_subfolder)
+        self.copy("cppzmq_extra.cmake", dst=os.path.join("lib", "cmake", "cppzmq"))
 
     def package_id(self):
         self.info.header_only()
 
     def package_info(self):
-        self.cpp_info.builddir = [os.path.join(self.package_folder, "lib", "cmake", "cppzmq")]
-        self.cpp_info.build_modules = [os.path.join(self.package_folder, "lib", "cmake", "cppzmq", "cppzmq_extra.cmake")]
+        self.cpp_info.builddir = [os.path.join("lib", "cmake", "cppzmq")]
+        self.cpp_info.build_modules = [os.path.join("lib", "cmake", "cppzmq", "cppzmq_extra.cmake")]
