99e43dc4ef09fa9f7bf973f60916e1ce5ae79c46
Luis <lasote@gmail.com>
2022-01-31 09:23:14 +0100

    try

--- recipes/zlib/all/conanfile.py
+++ recipes/zlib/all/conanfile.py
@@ -1,5 +1,10 @@
-from conans import ConanFile, tools, CMake
+from conan import ConanFile
+from conan.tools import Version
+from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.files import get, patch, rename, chdir, load, save
+from conans.tools import replace_in_file
 import os
+
 
 required_conan_version = ">=1.43.0"
 
@@ -12,7 +17,6 @@ class ZlibConan(ConanFile):
     description = ("A Massively Spiffy Yet Delicately Unobtrusive Compression Library "
                    "(Also Free, Not to Mention Unencumbered by Patents)")
     topics = ("zlib", "compression")
-
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
@@ -23,20 +27,15 @@ class ZlibConan(ConanFile):
         "fPIC": True,
     }
 
-    generators = "cmake"
+    generators = "CMakeToolchain"
 
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
+    def layout(self):
+        cmake_layout(self)
+        # self.folders.source = "source_subfolder"
 
     def export_sources(self):
-        self.copy("CMakeLists.txt")
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            self.copy(patch["patch_file"])
+        for _patch in self.conan_data.get("patches", {}).get(self.version, []):
+            self.copy(_patch["patch_file"])
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -49,35 +48,34 @@ class ZlibConan(ConanFile):
         del self.settings.compiler.cppstd
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
 
     def _patch_sources(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
+        for _patch in self.conan_data.get("patches", {}).get(self.version, []):
+            # !!!! This is not easy to understand, the patches are un the base source folder, not an easy access. Here the current dir is source_folder.
+            patch(self, patch_file=os.path.join(self.folders.base_source, _patch["patch_file"]))
 
-        with tools.chdir(self._source_subfolder):
-            # https://github.com/madler/zlib/issues/268
-            tools.replace_in_file('gzguts.h',
-                                  '#if defined(_WIN32) || defined(__CYGWIN__)',
-                                  '#if defined(_WIN32) || defined(__MINGW32__)')
+        # https://github.com/madler/zlib/issues/268
+        replace_in_file(os.path.join(self.source_folder, 'gzguts.h'), '#if defined(_WIN32) || defined(__CYGWIN__)', '#if defined(_WIN32) || defined(__MINGW32__)')
 
-            is_apple_clang12 = self.settings.compiler == "apple-clang" and tools.Version(self.settings.compiler.version) >= "12.0"
-            if not is_apple_clang12:
-                for filename in ['zconf.h', 'zconf.h.cmakein', 'zconf.h.in']:
-                    tools.replace_in_file(filename,
-                                          '#ifdef HAVE_UNISTD_H    '
-                                          '/* may be set to #if 1 by ./configure */',
-                                          '#if defined(HAVE_UNISTD_H) && (1-HAVE_UNISTD_H-1 != 0)')
-                    tools.replace_in_file(filename,
-                                          '#ifdef HAVE_STDARG_H    '
-                                          '/* may be set to #if 1 by ./configure */',
-                                          '#if defined(HAVE_STDARG_H) && (1-HAVE_STDARG_H-1 != 0)')
+        is_apple_clang12 = self.settings.compiler == "apple-clang" and Version(str(self.settings.compiler.version)) >= "12.0"
+        if not is_apple_clang12:
+            for filename in ['zconf.h', 'zconf.h.cmakein', 'zconf.h.in']:
+                filename = os.path.join(self.source_folder, filename)
+                replace_in_file(filename,
+                                      '#ifdef HAVE_UNISTD_H    '
+                                      '/* may be set to #if 1 by ./configure */',
+                                      '#if defined(HAVE_UNISTD_H) && (1-HAVE_UNISTD_H-1 != 0)')
+                replace_in_file(filename,
+                                      '#ifdef HAVE_STDARG_H    '
+                                      '/* may be set to #if 1 by ./configure */',
+                                      '#if defined(HAVE_STDARG_H) && (1-HAVE_STDARG_H-1 != 0)')
 
     def build(self):
         self._patch_sources()
         make_target = "zlib" if self.options.shared else "zlibstatic"
         cmake = CMake(self)
-        cmake.configure(build_folder=self._build_subfolder)
+        cmake.configure()
         cmake.build(target=make_target)
 
     def _rename_libraries(self):
@@ -88,48 +86,50 @@ class ZlibConan(ConanFile):
             if self.options.shared:
                 if self.settings.compiler == "Visual Studio" and suffix:
                     current_lib = os.path.join(lib_path, "zlib%s.lib" % suffix)
-                    tools.rename(current_lib, os.path.join(lib_path, "zlib.lib"))
+                    rename(current_lib, os.path.join(lib_path, "zlib.lib"))
             else:
                 if self.settings.compiler == "Visual Studio":
                     current_lib = os.path.join(lib_path, "zlibstatic%s.lib" % suffix)
-                    tools.rename(current_lib, os.path.join(lib_path, "zlib.lib"))
+                    rename(current_lib, os.path.join(lib_path, "zlib.lib"))
                 elif self.settings.compiler in ("clang", "gcc", ):
                     if not self.settings.os.subsystem:
                         current_lib = os.path.join(lib_path, "libzlibstatic.a")
-                        tools.rename(current_lib, os.path.join(lib_path, "libzlib.a"))
+                        rename(current_lib, os.path.join(lib_path, "libzlib.a"))
 
     def _extract_license(self):
-        with tools.chdir(os.path.join(self.source_folder, self._source_subfolder)):
-            tmp = tools.load("zlib.h")
+        with chdir(self, os.path.join(self.source_folder)):
+            tmp = load(self, "zlib.h")
             license_contents = tmp[2:tmp.find("*/", 1)]
-            tools.save("LICENSE", license_contents)
+            save(self, "LICENSE", license_contents)
 
     def package(self):
         self._extract_license()
-        self.copy("LICENSE", src=self._source_subfolder, dst="licenses")
+        self.copy("LICENSE", src=self.source_folder, dst="licenses")
 
         # Copy headers
         for header in ["*zlib.h", "*zconf.h"]:
-            self.copy(pattern=header, dst="include", src=self._source_subfolder, keep_path=False)
-            self.copy(pattern=header, dst="include", src=self._build_subfolder, keep_path=False)
+            self.copy(pattern=header, dst="include", src=self.source_folder, keep_path=False)
+            self.copy(pattern=header, dst="include", src=self.build_folder, keep_path=False)
 
         # Copying static and dynamic libs
         if self.options.shared:
-            self.copy(pattern="*.dylib*", dst="lib", src=self._build_subfolder, keep_path=False, symlinks=True)
-            self.copy(pattern="*.so*", dst="lib", src=self._build_subfolder, keep_path=False, symlinks=True)
-            self.copy(pattern="*.dll", dst="bin", src=self._build_subfolder, keep_path=False)
-            self.copy(pattern="*.dll.a", dst="lib", src=self._build_subfolder, keep_path=False)
+            self.copy(pattern="*.dylib*", dst="lib", src=self.build_folder, keep_path=False, symlinks=True)
+            self.copy(pattern="*.so*", dst="lib", src=self.build_folder, keep_path=False, symlinks=True)
+            self.copy(pattern="*.dll", dst="bin", src=self.build_folder, keep_path=False)
+            self.copy(pattern="*.dll.a", dst="lib", src=self.build_folder, keep_path=False)
         else:
-            self.copy(pattern="*.a", dst="lib", src=self._build_subfolder, keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", src=self._build_subfolder, keep_path=False)
+            self.copy(pattern="*.a", dst="lib", src=self.build_folder, keep_path=False)
+        self.copy(pattern="*.lib", dst="lib", src=self.build_folder, keep_path=False)
 
         self._rename_libraries()
 
     def package_info(self):
+        self.cpp_info.builddirs = []
         self.cpp_info.set_property("cmake_file_name", "ZLIB")
         self.cpp_info.set_property("cmake_target_name", "ZLIB::ZLIB")
         self.cpp_info.set_property("cmake_find_mode", "both")
         self.cpp_info.libs.append("zlib" if self.settings.os == "Windows" and not self.settings.os.subsystem else "z")
 
-        self.cpp_info.names["cmake_find_package"] = "ZLIB"
-        self.cpp_info.names["cmake_find_package_multi"] = "ZLIB"
+        # !!! .names has to be mocked
+        # self.cpp_info.names["cmake_find_package"] = "ZLIB"
+        # self.cpp_info.names["cmake_find_package_multi"] = "ZLIB"
