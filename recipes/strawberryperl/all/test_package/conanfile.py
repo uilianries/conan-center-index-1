6de04d6bed45ab4e477c71e012916752dcbd0809
Luis Martinez de Bartolome <lasote@gmail.com>
2022-03-07 17:52:25 +0100

    2.0 compatible recipe

--- recipes/strawberryperl/all/test_package/conanfile.py
+++ recipes/strawberryperl/all/test_package/conanfile.py
@@ -1,12 +1,20 @@
 import os
-from conans import ConanFile, tools
+from conan import ConanFile
+from conan.tools.build import cross_building
+from conan.tools.layout import basic_layout
 
 
 class DefaultNameConan(ConanFile):
     settings = "os", "arch"
 
+    def layout(self):
+        basic_layout(self)
+
+    def requirements(self):
+        self.tool_requires(self.tested_reference_str)
+
     def test(self):
-        if not tools.cross_building(self):
-            self.run("perl --version", run_environment=True)
+        if not cross_building(self):
+            self.run("perl --version")
             perl_script = os.path.join(self.source_folder, "list_files.pl")
-            self.run("perl {}".format(perl_script), run_environment=True)
+            self.run("perl {}".format(perl_script))
