d2ea5d4530253b7b18d9fbe3ca453094abfd108a
Uilian Ries <uilianries@gmail.com>
2022-06-23 13:25:24 +0200

    Remove CMake requirement from test package
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/qcoro/all/conanfile.py
+++ recipes/qcoro/all/conanfile.py
@@ -54,9 +54,6 @@ class QCoroConan(ConanFile):
         if self.options.shared:
             del self.options.fPIC
 
-    def build_requirements(self):
-        self.build_requires("cmake/3.22.0")
-
     def requirements(self):
         self.requires("qt/6.2.2")
 
